b013e566b28de51a57b94e9f98b420082af7e062
Anas Saeed <anassaeed@Anass-MacBook-Pro.local>
2016-05-23 22:18:42 -0400

    Brushing

---
+++ Brushing/src/com/anassaeed/brushing/One.java
@@ -0,0 +1,54 @@
+package com.anassaeed.brushing;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+
+
+public class One extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.one);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+    // TODO Auto-generated method stub
+    	int eventaction=event.getAction();
+
+        switch(eventaction) {
+  
+          case MotionEvent.ACTION_DOWN:
+        	  Intent intent = new Intent(One.this, Two.class);
+        	  startActivity(intent);
+              break;
+     
+          default:
+              break;
+        }
+    return super.onTouchEvent(event);
+     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
