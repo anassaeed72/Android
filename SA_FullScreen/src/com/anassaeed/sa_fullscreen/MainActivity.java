fc12173c5e1c23cdf28a16a80d7502fb2e038a15
Anas Saeed <anassaeed@Anass-MacBook-Pro.local>
2016-05-23 22:32:23 -0400

    SA_FullScreen

---
+++ SA_FullScreen/src/com/anassaeed/sa_fullscreen/MainActivity.java
@@ -0,0 +1,67 @@
+package com.anassaeed.sa_fullscreen;
+
+
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	public void FullScreencall() {
+
+	    if(Build.VERSION.SDK_INT < 19){ //19 or above api
+	        View v = this.getWindow().getDecorView();
+	        v.setSystemUiVisibility(View.GONE);
+	    } else {
+	            //for lower api versions.
+	        View decorView = getWindow().getDecorView(); 
+	        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+	        decorView.setSystemUiVisibility(uiOptions);
+	    }
+	}
+	private void executeDelayed() {
+	    Handler handler = new Handler();
+	    handler.postDelayed(new Runnable() {
+	        @Override
+	        public void run() {
+	            // execute after 500ms
+	            FullScreencall();
+	        }
+	    }, 500);
+	}
+	@Override
+	protected void onResume() {
+	    super.onResume();
+	    executeDelayed();
+	}	
+}
