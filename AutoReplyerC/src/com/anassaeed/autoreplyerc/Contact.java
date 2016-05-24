8fd8e53f51fa2822b3d63109b5a40b339d97a24e
Anas Saeed <anassaeed@Anass-MacBook-Pro.local>
2016-05-23 22:16:53 -0400

    AutoReplyerC

---
+++ AutoReplyerC/src/com/anassaeed/autoreplyerc/Contact.java
@@ -0,0 +1,64 @@
+package com.anassaeed.autoreplyerc;
+
+public class Contact {
+    
+    //private variables
+    int _id;
+    String _name;
+    String _phone_number;
+    int _times;
+    // Empty constructor
+    public Contact(){
+         
+    }
+    // constructor
+    public Contact(int id, String name, String _phone_number,int _times){
+        this._id = id;
+        this._name = name;
+        this._phone_number = _phone_number;
+        this._times = _times;
+    }
+     
+    // constructor
+    public Contact(String name, String _phone_number, int _times){
+        this._name = name;
+        this._phone_number = _phone_number;
+        this._times = _times;
+    }
+    // getting ID
+    public int getID(){
+        return this._id;
+    }
+     
+    // setting id
+    public void setID(int id){
+        this._id = id;
+    }
+     
+    // getting name
+    public String getName(){
+        return this._name;
+    }
+     
+    // setting name
+    public void setName(String name){
+        this._name = name;
+    }
+     
+    // getting phone number
+    public String getPhoneNumber(){
+        return this._phone_number;
+    }
+     
+    // setting phone number
+    public void setPhoneNumber(String phone_number){
+        this._phone_number = phone_number;
+    }
+    public int getTimes(){
+    	return this._times;
+    }
+    public void setTimes(int times){
+    	this._times = times;
+    }
+    
+}
---
+++ AutoReplyerC/src/com/anassaeed/autoreplyerc/ViewAllContacts.java
@@ -0,0 +1,5 @@
+package com.anassaeed.autoreplyerc;
+
+public class ViewAllContacts {
+
+}
