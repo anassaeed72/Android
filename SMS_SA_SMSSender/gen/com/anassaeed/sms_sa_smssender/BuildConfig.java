d2c341999d43072ac1aefe828678e259e7d12213
Anas Saeed <anassaeed@Anass-MacBook-Pro.local>
2016-05-23 22:33:58 -0400

    SMS_SA_SMSSender

---
+++ SMS_SA_SMSSender/gen/com/anassaeed/sms_sa_smssender/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.anassaeed.sms_sa_smssender;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ SMS_SA_SMSSender/src/com/anassaeed/sms_sa_smssender/MainActivity.java
@@ -0,0 +1,38 @@
+package com.anassaeed.sms_sa_smssender;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+	String messageContent = "Message";
+	String messageNumber = "03234089556";
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(messageNumber, null, messageContent, null, null);    				
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
